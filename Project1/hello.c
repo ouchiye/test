fa0d3aeb47d8c09f5e865ede684165d653c5ee1c
ou <1047658625@qq.com>
2021-07-13 19:45:24 +0800

    水时间

---
+++ Project1/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+int main(void)
+{
+	printf("Hello VS2019\n");
+//	system("pause");
+//	system("calc");
+//	system("mspaint");
+	Sleep(5000);
+	system("cls");
+	return 0;
+}
